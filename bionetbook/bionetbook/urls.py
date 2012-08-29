ad21e8a2f669e8700765d3023cbef069f8f3dc3b
Daniel Greenfeld <pydanny@gmail.com>
2012-08-29 16:16:02 -0700

    Final template fun

--- bionetbook/bionetbook/urls.py
+++ bionetbook/bionetbook/urls.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
+    url(regex=r'^$',
+        view=TemplateView.as_view(template_name="home.html"),
+        name="home"
+        ),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
