194eaa6259805e742d1072a3eb17b2a377bcbf23
Daniel Greenfeld <pydanny@gmail.com>
2012-08-30 14:35:35 -0700

    Considering protocol objects

---
+++ bionetbook/protocols/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import patterns, url
+
+from protocols import views
+
+urlpatterns = patterns("",
+
+    url(regex=r'^new/$',
+        view=views.ProtocolCreateView.as_view(),
+        name='protocol_create'),
+    url(regex=r'^(?P<slug>[-\w]+)/$',
+        view=views.ProtocolDetailView.as_view(),
+        name='protocol_detail'),
+    url(regex=r'^$',
+        view=views.ProtocolListView.as_view(),
+        name='protocol_list'),
+)
--- bionetbook/protocols/views.py
+++ bionetbook/protocols/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
+
+from protocols.models import Protocol
+
+
+class ProtocolDetailView(DetailView):
+
+    model = Protocol
+
+
+class ProtocolListView(ListView):
+
+    model = Protocol
+
+
+class ProtocolCreateView(CreateView):
+
+    model = Protocol
