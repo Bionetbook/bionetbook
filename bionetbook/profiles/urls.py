e8ef04e676c6a2d9b835425fdc99ac8e3f50008b
Daniel Greenfeld <pydanny@gmail.com>
2012-09-05 12:52:49 -0700

    Getting profiles to work again

--- bionetbook/profiles/urls.py
+++ bionetbook/profiles/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls.defaults import patterns, url
 
 from profiletools import views
 
+from profiles.views import ProfileUpdateView
+
 urlpatterns = patterns("",
+    url(regex=r'^$',
+        view=views.LoginRequireProfileListView.as_view(),
+        name='profile_list'),
     url(regex=r'^mine/$',
-        view=views.DefaultProfileUpdateNoSlugView.as_view(),
+        view=ProfileUpdateView.as_view(),
         name='profile_update'),
     url(regex=r'^(?P<pk>\d+)/$',
         view=views.ProfileDetailView.as_view(),
--- bionetbook/profiles/views.py
+++ bionetbook/profiles/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.views.generic import UpdateView
+
+from braces.views import LoginRequiredMixin
+
+from profiles.forms import ProfileForm
+from profiles.models import Profile
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Profile
+    form_class = ProfileForm
+
+    def get_object(self):
+        profile, created = Profile.objects.get_or_create(
+            user=self.request.user
+        )
+        return profile
