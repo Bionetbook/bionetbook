87e7c6a660a7c7dd0e3917baddcec444f02ade86
Daniel Greenfeld <pydanny@gmail.com>
2012-09-20 00:17:48 +0200

    Protocol view coverage bump

--- bionetbook/protocols/tests/test_views.py
+++ bionetbook/protocols/tests/test_views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from profiles.models import Profile
 from protocols.models import Protocol
+from steps.models import Step
 
 
 class ProtocolViewTests(TestCase):
@@ -14,12 +16,33 @@ class ProtocolViewTests(TestCase):
             password="password",
             email="test@example.com"
             )
+        self.profile = Profile.objects.create(
+            user=self.user
+        )
+        self.protocol = Protocol.objects.create(
+            name = "Test Protocol",
+            owner = self.user
+        )
+        self.protocol.save()
+        self.step = Step.objects.create(
+            name = "Test Step",
+            protocol=self.protocol
+        )
+        self.step.save()        
 
     def test_create_protocol(self):
 
         url = reverse("protocol_create")
         self.assertTrue(self.client.login(username='testuser', password='password'))
         
-        data = dict(name="test-protocol", raw="blag nlag")
+        data = dict(name="Test Protoco 2l", raw="blag nlag")
         response = self.client.post(url, data, follows=True)
-        print response
+        
+    def test_protocol_detail(self):
+        
+        self.assertTrue(self.client.login(username='testuser', password='password'))
+        url = self.protocol.get_absolute_url()
+        response = self.client.get(url)
+        self.assertContains(response, '<td><a href="/protocols/test-protocol/steps/test-step/">Test Step</a></td>')
+        
+        
