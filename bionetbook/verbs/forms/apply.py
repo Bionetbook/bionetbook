b4ee23cfaae5fae7550e803eb28014db1a6d92be
Daniel Greenfeld <pydanny@gmail.com>
2012-09-20 00:29:19 +0200

    Removing extra forms

--- bionetbook/verbs/forms/apply.py
+++
@@ -1,9 +0,0 @@
-from verbs.baseforms import forms
-
-
-class ApplyForm(forms.VerbForm):
-
-    name = "apply"
-    slug = "apply"
-
-    duration_in_seconds = forms.IntegerField()
--- bionetbook/verbs/forms/open.py
+++
@@ -1,9 +0,0 @@
-from verbs.baseforms import forms
-
-
-class OpenForm(forms.VerbForm):
-
-    name = "open"
-    slug = "open"
-
-    duration_in_seconds = forms.IntegerField()
