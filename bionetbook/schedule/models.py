fb8754e276c3c52fb04a1d214b824c67f27037f2
Grant Viklund <gviklund@backcode.com>
2012-10-14 19:16:01 -0700

    added custom save and slug generation

--- bionetbook/schedule/models.py
+++ bionetbook/schedule/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
-#from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse
 from django.db import models
-#from django.db.models import ObjectDoesNotExist
-#from django.template.defaultfilters import slugify
+from django.db.models import ObjectDoesNotExist
+from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
 from actions.models import Action
 
@@ -13,15 +13,50 @@ class Schedule(TimeStampedModel):
     owner = models.ForeignKey(User)
     start = models.TimeField()
     name = models.CharField(_("Name"), max_length=255)
+    uid = models.SlugField(_("UID"), blank=True, null=True, max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Event, self).save(*args, **kwargs)
+        if not self.uid:
+            uid = slugify("bnb-" + self.id)
+            try:
+                Event.objects.get(uid=uid)
+                self.uid = "{0}-{1}".format(uid, self.pk)
+            except ObjectDoesNotExist:
+                self.uid = uid
+            self.save()
+
+    #def get_absolute_url(self):
+    #    return reverse("schedule_detail", kwargs={'schedule_uid': self.uid})
 
 
 class Event(TimeStampedModel):
     child = models.ForeignKey("self", blank=True, null=True, unique=True)
     schedule = models.ForeignKey(Schedule)
     action = models.ForeignKey(Action)
-    summary = models.CharField(_("Summary"), max_length=255)
+    name = models.CharField(_("Summary"), max_length=255)
     description = models.TextField(blank=True, null=True)
     start = models.TimeField()
     end = models.TimeField()
-    uid = models.CharField(max_length=255)
+    uid = models.SlugField(_("UID"), blank=True, null=True, max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Event, self).save(*args, **kwargs)
+        if not self.uid:
+            uid = slugify(self.schedule.uid + "-" + self.id)
+            try:
+                Event.objects.get(uid=uid)
+                self.uid = "{0}-{1}".format(uid, self.pk)
+            except ObjectDoesNotExist:
+                self.uid = uid
+            self.save()
+
+    #def get_absolute_url(self):
+    #    return reverse("event_detail", kwargs={'event_uid': self.uid})
 
