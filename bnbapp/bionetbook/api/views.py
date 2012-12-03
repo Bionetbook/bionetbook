d46e2aaac4f4da1b9bde4368681b289ba6eb58a9
Grant Viklund <gviklund@backcode.com>
2012-12-02 17:48:05 -0800

    added error for non-existant protocol

--- bnbapp/bionetbook/api/views.py
+++ bnbapp/bionetbook/api/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 import django.utils.simplejson as json
 
@@ -6,5 +7,8 @@ from protocols.models import Protocol
 
 def protocol_detail(request, protocol_slug):
     if request.method == 'GET':
-		p = Protocol.objects.get(slug=protocol_slug)
-		return HttpResponse(json.dumps(p.data), mimetype="application/json")
+        try:
+            p = Protocol.objects.get(slug=protocol_slug)
+            return HttpResponse(json.dumps(p.data), mimetype="application/json")
+        except ObjectDoesNotExist:
+            return HttpResponse(json.dumps({'error':'ObjectDoesNotExist', 'description':'Could not find the requested protocol.'}), mimetype="application/json")
