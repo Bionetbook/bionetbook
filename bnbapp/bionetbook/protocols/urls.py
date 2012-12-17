12c39764b33bb73866f446954cbd8389df504eee
Grant Viklund <gviklund@backcode.com>
2012-12-16 16:38:55 -0800

    Updating creating steps

--- bnbapp/bionetbook/protocols/urls.py
+++ bnbapp/bionetbook/protocols/urls.py
@@ -3,18 +3,18 @@ from django.conf.urls.defaults import patterns, url, include
 from protocols import views
 
 urlpatterns = patterns("",
-    url(regex=r'^new/$', view=views.ProtocolCreateView.as_view(), name='protocol_create'),
-    #url(regex=r'^(?P<protocol_slug>[-\w]+)/steps/', include('steps.urls')),
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/$', view=views.ProtocolDetailView.as_view(), name='protocol_detail'),
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/edit/$', view=views.ProtocolUpdateView.as_view(), name='protocol_update'),
+
     url(regex=r'^$', view=views.ProtocolListView.as_view(), name='protocol_list'),
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/publish/$', view=views.ProtocolPublishView.as_view(), name='protocol_publish'),
 
     # PROTOCOL URLS
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/steps/$', view=views.StepListView.as_view(), name='step_list'),
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/create/$', view=views.ProtocolDetailView.as_view(), name='step_create'),
+    url(regex=r'^create/$', view=views.ProtocolCreateView.as_view(), name='protocol_create'),
+    url(regex=r'^(?P<protocol_slug>[-\w]+)/$', view=views.ProtocolDetailView.as_view(), name='protocol_detail'),
+    url(regex=r'^(?P<protocol_slug>[-\w]+)/edit/$', view=views.ProtocolUpdateView.as_view(), name='protocol_update'),
+    url(regex=r'^(?P<protocol_slug>[-\w]+)/publish/$', view=views.ProtocolPublishView.as_view(), name='protocol_publish'),
 
     # STEP URLS
+    #url(regex=r'^(?P<protocol_slug>[-\w]+)/steps/$', view=views.StepListView.as_view(), name='step_list'),
+    url(regex=r'^(?P<protocol_slug>[-\w]+)/create/$', view=views.StepCreateView.as_view(), name='step_create'),
     url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/$', view=views.StepDetailView.as_view(), name='step_detail'),
     url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/update/$', view=views.StepDetailView.as_view(), name='step_update'),
 
