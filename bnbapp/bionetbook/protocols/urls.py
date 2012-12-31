ecdb39942044b95965da47db5edbc8dc4d13c0a2
Grant Viklund <gviklund@backcode.com>
2012-12-31 13:47:31 -0800

    clenaup pass on forms and URLs

--- bnbapp/bionetbook/protocols/urls.py
+++ bnbapp/bionetbook/protocols/urls.py
@@ -20,9 +20,7 @@ urlpatterns = patterns("",
 
     # ACTION URLS
     url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/add-action/$', view=views.ActionVerbListView.as_view(), name='action_verb_list'),
-    url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/add-action/(?P<verb_slug>[-\w]+)/$', view=views.ActionCreateView.as_view(), name='action_verb_create'),
+    url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/add-action/(?P<verb_slug>[-\w]+)/$', view=views.ActionCreateView.as_view(), name='action_create'),
     url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/(?P<action_slug>[-\w]+)/$', view=views.ActionDetailView.as_view(), name='action_detail'),
     url(regex=r'^(?P<protocol_slug>[-\w]+)/(?P<step_slug>[-\w]+)/(?P<action_slug>[-\w]+)/update/$', view=views.ActionUpdateView.as_view(), name='action_update'),
 )
-
-
