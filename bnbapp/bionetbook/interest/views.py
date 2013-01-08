bce857f54e4de4b69ad0dcb6c17189659cba63e0
Grant Viklund <gviklund@backcode.com>
2013-01-07 22:49:08 -0800

    better message for interest list added

--- bnbapp/bionetbook/interest/views.py
+++ bnbapp/bionetbook/interest/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+
 from django.views.generic import FormView
 from django.views.generic.edit import ModelFormMixin, CreateView
 
@@ -17,3 +19,7 @@ class InterestCreate(CreateView):
     #    form.send_email()
     #    return super(ContactView, self).form_valid(form)
 
+    def form_valid(self, form):
+        result = super(InterestCreate, self).form_valid(form)
+        messages.add_message(self.request, messages.INFO, "Your email has been added to our interest list!")
+        return result
