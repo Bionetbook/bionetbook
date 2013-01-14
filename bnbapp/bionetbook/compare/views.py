adea35af86fca5c7b04c8c175e490566d502313e
Grant Viklund <gviklund@backcode.com>
2013-01-13 16:54:10 -0800

    added protocol_plot view to compare

--- bnbapp/bionetbook/compare/views.py
+++ bnbapp/bionetbook/compare/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def protocol_plot(request, protocol_slug):
+
+	svg_plot = None
+	# GET THE PROTOCOL
+
+	# GET THE PLOT
+    #svg_data = generate_some_svg_data()
+
+    # RETURN THE PLOT
+    return HttpResponse(svg_data, mimetype="image/svg+xml")
