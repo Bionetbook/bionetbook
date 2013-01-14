55a0138bf7dcca6a10c3019a9e79d5c82deb3000
orenis1 <orenis1@gmail.com>
2013-01-14 10:44:41 -0800

    edited component for reagent, still missing some items to finish this

--- bnbapp/bionetbook/protocols/models.py
+++ bnbapp/bionetbook/protocols/models.py
@@ -299,27 +299,66 @@ class Protocol(TimeStampedModel):
             mass = dict((k, v) for k, v in units_dict.iteritems() if 'mass' in k)
 
             if conc:
-                if conc['max_conc'] == conc['min_conc']:
-                    units = conc['max_conc']
+                # check that all data is present:
+                if not conc['conc_units']
+                    return 'no concentration units specified for %s' % units_dict['name']
+                if not conc['max_conc'] and not conc['min_conc']:
+                    return 'enter concentration units for %s' % units_dict['name']
+
+                if conc['max_conc'] and conc['min_conc']:
+
+                    if conc['max_conc'] == conc['min_conc']:
+                        units = conc['max_conc']
+                    else:
+                        units = units + conc['min_conc'] + '-' + conc['max_conc']
                 else:
-                    units = units + conc['min_conc'] + '-' + conc['max_conc']           
-                
+                    if conc['max_conc']:
+                         units = conc['max_conc']
+                    if conc['min_conc']:
+                         units = conc['min_conc']                        
+                    
                 units = units + ' ' + conc['conc_units']  
 
             
             if vol:
-                if vol['max_vol'] == vol['min_vol']:
-                    units = units + ', ' + vol['max_vol']
+                if not vol['vol_units']
+                    return 'no concentration units specified for %s' % units_dict['name']
+                if not vol['max_vol'] and not vol['min_vol']:
+                    return 'enter concentration units for %s' % units_dict['name']
+
+                if vol['max_vol'] and vol['min_vol']:        
+
+                    if vol['max_vol'] == vol['min_vol']:
+                        units = units + ', ' + vol['max_vol']
+                    else:
+                        units = units + ', ' + vol['min_vol'] + '-' + vol['max_vol']
+
                 else:
-                    units = units + ', ' + vol['min_vol'] + '-' + vol['max_vol']               
+                    if vol['max_vol']:
+                         units = vol['max_vol']
+                    if vol['min_vol']:
+                         units = vol['min_vol']          
                 
                 units = units + ' ' + vol['vol_units'] 
 
             if mass:
-                if mass['min_mass'] == mass['max_mass']:
-                    units = units + ', ' + mass['max_mass']
+                if not mass['mass_units']
+                    return 'no concentration units specified for %s' % units_dict['name']
+                if not mass['max_mass'] and not mass['min_mass']:
+                    return 'enter concentration units for %s' % units_dict['name']
+
+                if mass['max_mass'] and mass['min_mass']:
+
+                    if mass['min_mass'] == mass['max_mass']:
+                        units = units + ', ' + mass['max_mass']
+                    else:
+                        units = units + ', ' + mass['min_mass'] + '-' + mass['max_mass']
+
                 else:
-                    units = units + ', ' + mass['min_mass'] + '-' + mass['max_mass']
+                    if mass['max_mass']:
+                         units = mass['max_mass']
+                    if mass['min_mass']:
+                         units = mass['min_mass']
 
                 units = units + ' ' + mass['mass_units']
 
@@ -585,9 +624,8 @@ class NodeBase(dict):
         return self.protocol.name
 
 
-class Componenet(NodeBase):
+class Component(NodeBase):
     pass
-
 
 class Action(NodeBase):
 
