acd61495c42659021fb67014428f39844f6d17ad
Grant Viklund <gviklund@backcode.com>
2013-01-13 19:44:16 -0800

    Working on Organization View

--- bnbapp/bionetbook/organization/urls.py
+++ bnbapp/bionetbook/organization/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls.defaults import patterns, url, include
 from organization import views
 
 urlpatterns = patterns("",
-#    url(regex=r'^$', view=views.SomeView.as_view(), name='organization_view'),
+    url(regex=r'^(?P<slug>[-_\w]+)/$', view=views.OrganizationDetailView.as_view(), name='organization_detail'),
 )
-
-
