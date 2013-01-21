908c16beb69ab9a9887d9e53cc09029395e9fcd4
Grant Viklund <gviklund@backcode.com>
2013-01-20 17:53:13 -0800

    Working on Protocol Create form to include protocols only from a given organization.

--- bnbapp/bionetbook/protocols/forms/__init__.py
+++ bnbapp/bionetbook/protocols/forms/__init__.py
@@ -1,5 +1,6 @@
 import floppyforms as forms
 from protocols.models import Protocol
+from organization.models import Organization
 
 class ProtocolForm(forms.ModelForm):
 
@@ -8,13 +9,17 @@ class ProtocolForm(forms.ModelForm):
         exclude = ('parent', 'slug', 'duration_in_seconds', 'status', 'version','raw')
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
+        #user = kwargs.pop('user', None)
         super(ProtocolForm, self).__init__(*args, **kwargs)
         #choices=[]
+        print kwargs
+
+        #print user
         #choices = [(pt.id, unicode(pt.name)) for pt in PromotionType.objects.all()]
         #choices.extend(EXTRA_CHOICES)
-        #self.fields['owner'].choices = choices
-        self.fields['owner'].queryset = user.organization_set.all()
+        choices = [(pt.id, unicode(pt.name)) for pt in Organization.objects.all()]
+        self.fields['owner'].choices = choices
+        #self.fields['owner'].queryset = user.organization_set.all()
 
 
 
