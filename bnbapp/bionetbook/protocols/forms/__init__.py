d97a6529568712e835c3c70f12325b3f324b16f0
Grant Viklund <gviklund@backcode.com>
2013-01-22 19:16:08 -0800

    Moved Protocol form in to baseforms.py

--- bnbapp/bionetbook/protocols/forms/__init__.py
+++ bnbapp/bionetbook/protocols/forms/__init__.py
@@ -1,27 +1,6 @@
 import floppyforms as forms
-from protocols.models import Protocol
-from organization.models import Organization
 
-class ProtocolForm(forms.ModelForm):
-
-    class Meta:
-        model = Protocol
-        exclude = ('parent', 'slug', 'duration_in_seconds', 'status', 'version','raw')
-
-    def __init__(self, *args, **kwargs):
-        #user = kwargs.pop('user', None)
-        super(ProtocolForm, self).__init__(*args, **kwargs)
-        #choices=[]
-        print kwargs
-
-        #print user
-        #choices = [(pt.id, unicode(pt.name)) for pt in PromotionType.objects.all()]
-        #choices.extend(EXTRA_CHOICES)
-        choices = [(pt.id, unicode(pt.name)) for pt in Organization.objects.all()]
-        self.fields['owner'].choices = choices
-        #self.fields['owner'].queryset = user.organization_set.all()
-
-
+from protocols.forms.baseforms import ProtocolForm
 
 
 class PublishForm(forms.Form):
