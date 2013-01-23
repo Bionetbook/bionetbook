65b84abb00a24c14bc86db195c8409f4aa3a2b2e
Grant Viklund <gviklund@backcode.com>
2013-01-22 19:36:41 -0800

    Cleaned up cruft

--- bnbapp/bionetbook/protocols/forms/baseforms.py
+++ bnbapp/bionetbook/protocols/forms/baseforms.py
@@ -9,14 +9,6 @@ class ProtocolForm(forms.ModelForm):
         exclude = ('parent', 'slug', 'duration_in_seconds', 'status', 'version','raw')
 
     def __init__(self, *args, **kwargs):
-        #user = kwargs.pop('user', None)
         super(ProtocolForm, self).__init__(*args, **kwargs)
-        #choices=[]
-        print kwargs
-
-        #print user
-        #choices = [(pt.id, unicode(pt.name)) for pt in PromotionType.objects.all()]
-        #choices.extend(EXTRA_CHOICES)
         choices = [(pt.id, unicode(pt.name)) for pt in Organization.objects.all()]
         self.fields['owner'].choices = choices
-        #self.fields['owner'].queryset = user.organization_set.all()
