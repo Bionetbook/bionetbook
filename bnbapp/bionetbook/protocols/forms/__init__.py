567ca3d5b2a8f1fc7397733fdeaf984e80345abb
Grant Viklund <gviklund@backcode.com>
2013-01-22 19:52:40 -0800

    Functionally working protocols

--- bnbapp/bionetbook/protocols/forms/__init__.py
+++ bnbapp/bionetbook/protocols/forms/__init__.py
@@ -2,6 +2,20 @@ import floppyforms as forms
 
 #from protocols.forms.baseforms import ProtocolForm
 
+from protocols.models import Protocol
+from organization.models import Organization
+
+class ProtocolForm(forms.ModelForm):
+
+    class Meta:
+        model = Protocol
+        exclude = ('parent', 'slug', 'duration_in_seconds', 'status', 'version','raw')
+
+    def __init__(self, *args, **kwargs):
+        super(ProtocolForm, self).__init__(*args, **kwargs)
+        choices = [(pt.id, unicode(pt.name)) for pt in Organization.objects.all()]
+        self.fields['owner'].choices = choices
+
 
 class PublishForm(forms.Form):
     pass
