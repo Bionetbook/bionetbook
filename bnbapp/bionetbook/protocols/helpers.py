f478735eb991729d0791460bb5164aa7c316b874
orenis1 <orenis1@gmail.com>
2013-01-31 16:26:53 -0800

    modified protocols.helpers to include shorten function and trigger

--- bnbapp/bionetbook/protocols/helpers.py
+++ bnbapp/bionetbook/protocols/helpers.py
@@ -1,3 +1,41 @@
+def shorten(units, reverse = False):
+
+        d ={'nanograms':'ng',
+            'micrograms':'ug',    
+            'milligrams':'mg',    
+            'grams':'g',  
+            'kilograms':'kg', 
+            'nanoLiter':'ng', 
+            'microLiter':'ul',    
+            'microliter':'ul',    
+            'milliLiter':'ml',    
+            'Liters':'L',
+            'nanoMolar':'nM', 
+            'microMolar':'uM',    
+            'milliMolar':'mM',    
+            'Molar':'M',
+            'nanomole':'nm', 
+            'micromole':'um',    
+            'millimole':'mm',    
+            'mole':'m',
+            'nanograms/microLiter': 'ng/ul',
+            'Units/microliter': 'U/ul',
+            'Units': 'U',
+            'X':'X',
+            'minutes':'min', 
+            'seconds':'sec',    
+            'hour':'hr',    
+            'day':'d',  
+            'celsius':'C', 
+            'farenheit':'F', 
+            'kelvin':'K', 
+            }
+
+        if reverse:
+            pass
+        else:
+            return d[units]
+
 def settify(settings_dict, shorthand = True):
 
     settings = []
@@ -91,9 +129,13 @@ def settify(settings_dict, shorthand = True):
     return settings
 
 
-def unify(units_dict, shorthand = True):
+def unify(units_dict, shorthand = True, summary = False):
 
-    units = ''
+    units_c = ''
+    units_v = ''
+    units_m = ''
+    output = {}
+    # print summary
 
     conc = dict((k, v) for k, v in units_dict.iteritems() if 'conc' in k)
     vol = dict((k, v) for k, v in units_dict.iteritems() if 'vol' in k)
@@ -103,84 +145,117 @@ def unify(units_dict, shorthand = True):
         # check that all data is present:
         if not conc['conc_units']:
             return 'no concentration units specified for %s' % units_dict['name']
+        if shorthand:
+            conc_units = shorten(conc['conc_units'])      
         if 'max_conc' not in conc and 'min_conc' not in conc:
             return 'enter concentration units for %s' % units_dict['name']    
 
         if 'max_conc' in conc and 'min_conc' in conc:
 
             if conc['max_conc'] == conc['min_conc']:
-                units = conc['max_conc']
+                units_c = conc['max_conc']
             else:
-                units = units + conc['min_conc'] + '-' + conc['max_conc']
+                units_c = units_c + conc['min_conc'] + '-' + conc['max_conc']
         else:
             if 'max_conc' in conc:
-                 units = conc['max_conc']
+                 units_c = conc['max_conc']
             if 'min_conc' in conc:
-                 units = conc['min_conc']                        
-            
-        units = units + ' ' + conc['conc_units']  
+                 units_c = conc['min_conc']   
+
+        # print units_c                              
+        if summary:
+            output['conc'] = [units_c, conc_units]
+        else:    
+            units_c = units_c + ' ' + conc_units 
+        
 
     
     if vol:
         if not vol['vol_units']:
             return 'no Volume units specified for %s' % units_dict['name']
+        if shorthand:
+            vol_units = shorten(vol['vol_units'])  
+
+
         if 'max_vol' not in vol and 'min_vol' not in vol:
             return 'enter Volume units for %s' % units_dict['name']
 
         if 'max_vol' in vol and 'min_vol' in vol:
 
             if vol['max_vol'] == vol['min_vol']:
-                units = units + ', ' + vol['max_vol']
+                units_v = units_v + ', ' + vol['max_vol']
             else:
-                units = units + ', ' + vol['min_vol'] + '-' + vol['max_vol']
+                units_v = units_v + ', ' + vol['min_vol'] + '-' + vol['max_vol']
 
         else:
             if 'max_vol' in vol:
-                 units = vol['max_vol']
+                 units_v = vol['max_vol']
             if 'min_vol' in vol:
-                 units = vol['min_vol']          
+                 units_v = vol['min_vol']          
         
-        units = units + ' ' + vol['vol_units'] 
+        # print units_v
+        if summary:
+            output['vol'] = [units_v, vol_units]            
+        else:
+            units_v = units_v + ' ' + vol_units
+        
 
     if mass:
         if not mass['mass_units']:
             return 'no mass units specified for %s' % units_dict['name']
+        if shorthand:
+            mass_units = shorten(mass['mass_units'])
+
         if 'max_mass' not in mass and 'min_mass' not in mass:
             return 'enter mass units for %s' % units_dict['name']
 
         if 'max_mass' in mass and 'min_mass' in mass:
 
             if mass['min_mass'] == mass['max_mass']:
-                units = units + ', ' + mass['max_mass']
+                units_m = units_m + ', ' + mass['max_mass']
             else:
-                units = units + ', ' + mass['min_mass'] + '-' + mass['max_mass']
+                units_m = units_m + ', ' + mass['min_mass'] + '-' + mass['max_mass']
 
         else:
             if 'max_mass' in mass:
-                 units = mass['max_mass']
+                 units_m = mass['max_mass']
             if 'min_mass' in mass:
-                 units = mass['min_mass']
+                 units_m = mass['min_mass']
 
-        units = units + ' ' + mass['mass_units']
+        # print units_m         
+        if summary:
+            output['mass'] = [units_m, mass_units]
+        else:    
+            units_m = units_m + ' ' + mass_units
 
-    if shorthand == True:
-        units = units.replace('nanograms','ng') 
-        units = units.replace('micrograms','ug')    
-        units = units.replace('milligrams','mg')    
-        units = units.replace('grams','g')  
-        units = units.replace('kilograms','kg') 
-        units = units.replace('nanoLiter','ng') 
-        units = units.replace('microLiter','ul')    
-        units = units.replace('microliter','ul')    
-        units = units.replace('milliLiter','ml')    
-        units = units.replace('Liters','L')
-        units = units.replace('nanoMolar','nM') 
-        units = units.replace('microMolar','uM')    
-        units = units.replace('milliMolar','mM')    
-        units = units.replace('Molar','M')
-        units = units.replace('nanomole','nm') 
-        units = units.replace('micromole','um')    
-        units = units.replace('millimole','mm')    
-        units = units.replace('mole','m')
+        
 
-    return units
+    
+
+
+
+
+        # units_m = units_m.replace('nanograms','ng') 
+        # units_m = units_m.replace('micrograms','ug')    
+        # units_m = units_m.replace('milligrams','mg')    
+        # units_m = units_m.replace('grams','g')  
+        # units_m = units_m.replace('kilograms','kg') 
+        # units_v = units_v.replace('nanoLiter','ng') 
+        # units_v = units_v.replace('microLiter','ul')    
+        # units_v = units_v.replace('microliter','ul')    
+        # units_v = units_v.replace('milliLiter','ml')    
+        # units_v = units_v.replace('Liters','L')
+        # units_c = units_c.replace('nanoMolar','nM') 
+        # units_c = units_c.replace('microMolar','uM')    
+        # units_c = units_c.replace('milliMolar','mM')    
+        # units_c = units_c.replace('Molar','M')
+        # units_c = units_c.replace('nanomole','nm') 
+        # units_c = units_c.replace('micromole','um')    
+        # units_c = units_c.replace('millimole','mm')    
+        # units_c = units_c.replace('mole','m')
+
+    if summary:
+        # print '4th update'
+        return output
+    else:
+        return units_c + units_v + units_m
