6b8053a47ab565f1798c584495774ddf9d14f218
Grant Viklund <gviklund@backcode.com>
2013-02-17 15:25:32 -0800

    Added boilerplate for comparison

--- bnbapp/bionetbook/compare/views.py
+++ bnbapp/bionetbook/compare/views.py
@@ -1,7 +1,5 @@
-# from django.http import HttpResponse
-# from compare.models import ProtocolPlot
-# import pygraphviz as agraph
-from django.views.generic import TemplateView
+from compare.models import ProtocolPlot
+from django.views.generic import TemplateView, View
 
 
 class CompareView(TemplateView):
@@ -11,48 +9,26 @@ class CompareView(TemplateView):
         '''Gets the context data'''
         context = self.get_context_data()
 
-        context['protocol_a'] = kwargs['protocol_a_slug']
-        context['protocol_b'] = kwargs['protocol_b_slug']
+        context['protocol_a'] = ProtocolPlot.objects.get(slug=kwargs['protocol_a_slug'])
+        context['protocol_b'] = ProtocolPlot.objects.get(slug=kwargs['protocol_b_slug'])
 
         return self.render_to_response(context)
 
 
+class CompareGraphicView(View):
+    '''
+    Returns a graphic representaion of a comparison in either a SVG or PNG format.
+    '''
+
+    def get(self, request, *args, **kwargs):
+        '''Gets the context data'''
+        context = self.get_context_data()
+
+        protocol_a = ProtocolPlot.objects.get(slug=kwargs['protocol_a_slug'])
+        protocol_b = ProtocolPlot.objects.get(slug=kwargs['protocol_b_slug'])
+        format = kwargs['format']
 
 
-
-# def protocol_plot(request, protocol_slug):
-
-# 	# svg_plot = None
-# 	# GET THE PROTOCOL
-# 	protocol = ProtocolPlot.objects.get(slug__icontains=protocol_slug)
-	
-# 	# svg_data = protocol.get_svg()
-# 	# protocol.agraph.layout = 'dot'
-# 	# svg_data = protocol.agraph.draw(format='svg')
-
-
-# 	# GET THE PLOT
-# 	#svg_data = generate_some_svg_data()
-
-# 	# RETURN THE PLOT
-# 	# return HttpResponse(svg_data, mimetype="image/svg+xml")
-# 	# return HttpResponse(svg_data, mimetype="image/png")
-
-
-	
-# 	A=P.AGraph() # init empty graph
-# 	# set some default node attributes
-# 	# A.node_attr['style']='filled'
-# 	# A.node_attr['shape']='circle'
-# 	# Add edges (and nodes)
-# 	# A.add_edge(1,2)
-# 	# A.add_edge(2,3)
-# 	# A.add_edge(1,3)
-# 	# layout with default (neato)
-# 	svg_data = protocol.get_graph(A)
-
-# 	png=svg_data.draw(format='svg') # draw png
-# 	return HttpResponse(png, mimetype='image/svg+xml')
-
-
+        # NEED TO REPLACE THE BELOW RESPONSE WITH ONE USING STRINGIO
+        return self.render_to_response(context)
 
