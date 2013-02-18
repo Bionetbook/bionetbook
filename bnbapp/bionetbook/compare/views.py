0dc3c1233a13c49c832b9b3b2b9bcb6b95cef2b9
Grant Viklund <gviklund@backcode.com>
2013-02-17 16:11:59 -0800

    sperated out graph data from View

--- bnbapp/bionetbook/compare/views.py
+++ bnbapp/bionetbook/compare/views.py
@@ -23,26 +23,40 @@ class CompareGraphicView(View):
 
     def get(self, request, *args, **kwargs):
         '''Gets the context data'''
-        import pygraphviz as P
 
         protocol_a = ProtocolPlot.objects.get(slug=kwargs['protocol_a_slug'])
         protocol_b = ProtocolPlot.objects.get(slug=kwargs['protocol_b_slug'])
         format = kwargs['format']
 
-        A=P.AGraph() # init empty graph
-        # set some default node attributes
-        A.node_attr['style']='filled'
-        A.node_attr['shape']='circle'
-        # Add edges (and nodes)
-        A.add_edge(1,2)
-        A.add_edge(2,3)
-        A.add_edge(1,3)
-        A.layout() # layout with default (neato)
-        img=A.draw(format=format) # draw png
+        grapher = Grapher(protocol_a, protocol_b, format)
+        img = grapher.draw()
 
         if format in ['svg']:
             format = format + "+xml"
 
         response = HttpResponse(img, mimetype='image/%s' % format)
         return response
+
+
+class Grapher(object):
+    def __init__(self, protocol_a, protocol_b, format="svg"):
+        import pygraphviz as P
+
+        self.graph = P.AGraph()
+        self.protocol_a = protocol_a
+        self.protocol_b = protocol_b
+        self.format = format
+
+    def draw(self):
+        # set some default node attributes
+        self.graph.node_attr['style']='filled'
+        self.graph.node_attr['shape']='circle'
+        # Add edges (and nodes)
+        self.graph.add_edge(1,2)
+        self.graph.add_edge(2,3)
+        self.graph.add_edge(1,3)
+        self.graph.layout() # layout with default (neato)
+        return self.graph.draw(format=self.format) # draw png
+
+
 
