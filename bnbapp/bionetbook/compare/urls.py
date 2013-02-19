f87151520b081647c34d0cd4e9e1d546b97dae45
orenis1 <orenis1@gmail.com>
2013-02-19 12:52:56 -0800

    added url to compare layers

--- bnbapp/bionetbook/compare/urls.py
+++ bnbapp/bionetbook/compare/urls.py
@@ -4,7 +4,9 @@ from compare import views
 
 urlpatterns = patterns("",
     #url(regex=r'^(?P<protocol_slug>[-\w]+).svg$', view=protocol_plot, name='compare_protocol_plot'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.CompareView.as_view(), name='compare_protocols'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareGraphicView.as_view(), name='compare_protocol_graphic'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layer/(?P<layers>[-\w]+)$', view=views.CompareLayerView.as_view(), name='add_layers_graphic'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.CompareBaseView.as_view(), name='compare_protocols'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareBaseGraphicView.as_view(), name='compare_protocol_graphic'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layer/(?P<layers>[-\w]+)$', view=views.CompareLayersView.as_view(), name='add_layers'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layer/(?P<layers>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareLayersGraphicView.as_view(), name='add_layers_graphic'),
+
 )
