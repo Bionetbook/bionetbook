c284f421295e9cef3f86fbc5a338d9c89a921e51
orenis1 <orenis1@gmail.com>
2013-02-27 11:20:30 -0800

    added comments to forms

--- bnbapp/bionetbook/protocols/forms/verbs/combine.py
+++ bnbapp/bionetbook/protocols/forms/verbs/combine.py
@@ -10,6 +10,11 @@ class CombineForm(forms.VerbForm):
 
     min_temp = forms.IntegerField()
     max_temp = forms.IntegerField()
-    duration_min_time = forms.IntegerField()
+    duration_min_time = forms.IntegerField() # remove this
     describe_where = forms.CharField()
     edit_remarks = forms.CharField()
+
+    # add these:
+    # description = forms.CharField()
+    # add_reagents = forms.CharField(required = False, helptext='500 ng oligoDt | CaCl2, 5M 20 ml' )
+    # bring_volume_to = forms.CharField(required=False, helptext='12 ul, ddw')
--- bnbapp/bionetbook/protocols/forms/verbs/heat.py
+++ bnbapp/bionetbook/protocols/forms/verbs/heat.py
@@ -7,8 +7,13 @@ class HeatForm(forms.VerbForm):
     slug = "heat"
     has_machines = True
 
-    edit_what_remark = forms.CharField()
+    
+    edit_what_remark = forms.CharField(required = False, help_text='sample name, default is tube')
+    # machine = forms.CharField(required = False, help_text='enter machine name')
     min_temp = forms.IntegerField()
     max_temp = forms.IntegerField()
     duration_min_time = forms.IntegerField()
-    edit_remarks = forms.CharField()
+    # other_settings = forms.CharField(required = False)
+    edit_remarks = forms.CharField(required = False)
+	
+    
