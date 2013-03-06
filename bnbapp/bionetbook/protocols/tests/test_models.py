e88651774e8240c93089059ff567a574c84298ce
Grant Viklund <gviklund@backcode.com>
2013-03-05 18:34:08 -0800

    Added test code from previous version

--- bnbapp/bionetbook/protocols/tests/test_models.py
+++ bnbapp/bionetbook/protocols/tests/test_models.py
@@ -10,41 +10,60 @@ class ProtocolModelTests(TestCase):
 
     def setUp(self):
 
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="password",
-            email="test@example.com"
-            )
+        # CREATE THE USER
+        self.user = User.objects.create_user( username="testuser", password="password", email="test@example.com" )
         self.user.save()
-        self.organization = Organization.objects.create(name="testorg")
-        #self.user.organization.append(self.organization)
-        #print dir(self.organization.members)
-        #print self.organization.members
-        #self.organization.members.add(self.user)
-        #print dir(self.user)
+
+        # CREATE THE ORGANIZATION
+        self.org = Organization.objects.create(name="testorg")
+        self.org.save()
+
+        # ADD THE MEMBERSHIP
+        m = Membership(user=self.user, org=self.org)
+        m.save()
 
     def test_create_protocol(self):
-
-        protocol = Protocol.objects.create(
-            name="Test Protocol",
-            owner=self.organization,
-            raw="what?"
-            )
+        protocol = Protocol.objects.create( name="Test Protocol", owner=self.org, raw="what?" )
         self.assertEquals(protocol.raw, "what?")
+        self.assertEquals(protocol.slug, "test-protocol")
+
+    def test_protocol_keygen(self):
+        protocol = Protocol.objects.create( name="Test Protocol", owner=self.org, raw="what?" )
+        key1 = protocol.get_hash_id()
+        key2 = protocol.get_hash_id()
+        self.assertNotEqual(key1, key2)             # CONFIRM THAT THE KEYGEN IS UNIQUE EACH TIME
+
+    def test_step_add(self):
+        protocol = Protocol.objects.create( name="Test Protocol", owner=self.org, raw="what?" )
+        step1 = Step(protocol, name="step1")
+        self.assertEquals(len(protocol.steps), 1)   # STEP SHOULD REGEISTER IT'S SELF WITH THE PROTOCOL
+        self.assertTrue('objectid' in step1)        # STEP SHOULD GET OBJECT ID AFTER REGISTERED
+        self.assertEquals(step1['slug'], step1['objectid'])
+
+        step2 = Step(protocol, name="step2")
+        self.assertEquals(len(protocol.steps), 2)   # STEP SHOULD REGEISTER IT'S SELF WITH THE PROTOCOL
+        self.assertTrue('objectid' in step2)        # STEP SHOULD GET OBJECT ID AFTER REGISTERED
+        self.assertEquals(step2['slug'], step2['objectid'])
 
     def test_actions(self):
-        protocol = Protocol.objects.create(
-            name="Test Protocol",
-            owner=self.organization,
-            raw="what?"
-            )
-        step1 = Step(protocol=protocol, parent=protocol)
-        a1 = Action(protocol=protocol, parent=step1, verb="add")
-        # Action.objects.create(step=step1)
-        # Action.objects.create(step=step1)
-        # Action.objects.create(step=step1)
-        # step2 = Step.objects.create(protocol=protocol)
-        # Action.objects.create(step=step2)
-        # Action.objects.create(step=step2)
-        # Action.objects.create(step=step2)
-        # self.assertEquals(len(protocol.nodes), 2) # NEED TO TEST THIS OUT AND SEE WHY IT BREAKS
+        protocol = Protocol.objects.create( name="Test Protocol", owner=self.org, raw="what?" )
+
+        step1 = Step(protocol, name="step1")
+        # print "STEP 1 ID: %s" % step1['objectid']
+
+        # NEED TO MAKE TEST ACTION WITH EXPECTED FAILURE FOR NOT GETTING VERB ARGUMENT PASSED
+        # self.assertRaises(KeyError, Action, (protocol, parent=step1))
+
+        a1 = Action(protocol, parent=step1, data={'verb':"add"})
+        # print "ACTION 1 ID: %s" % a1['objectid']
+
+        # print "PROTOCOL DATA:"
+        # print protocol.data
+
+        # print "PROTOCOL NODES:"
+        # print protocol.nodes
+
+        self.assertEquals(len(step1.actions), 1)    # ASSERT THE ACTION IS ATTACHED TO THE PROTOCOL
+        self.assertTrue('objectid' in a1)           # ACTION SHOULD GET OBJECT ID AFTER REGISTERED
+        self.assertEquals(len(protocol.nodes), 2)   # ASSERT THAT THE ACTION WAS ADDED TO THE PROTOCOL AS WELL
+        self.assertEquals(a1['slug'], a1['objectid'])
--- bnbapp/bionetbook/protocols/tests/test_views.py
+++ bnbapp/bionetbook/protocols/tests/test_views.py
@@ -12,19 +12,33 @@ class ProtocolViewTests(TestCase):
 
     def setUp(self):
 
+        # USER SETUP
         self.user = User.objects.create_user(
             username="testuser",
             password="password",
             email="test@example.com"
             )
+
+        # USER PROFILE SETUP
         self.profile = Profile.objects.create(
             user=self.user
         )
-        self.organization = Organization.objects.create(name="testorg")
+
+        # CREATE THE ORGANIZATION
+        self.org = Organization.objects.create(name="testorg")
+        self.org.save()
+
+        # ADD THE MEMBERSHIP
+        m = Membership(user=self.user, org=self.org)
+        m.save()
+
+
+        # CREATE PROTOCOL
         self.protocol = Protocol.objects.create(
             name = "Test Protocol",
-            owner = self.organization
+            owner = self.org
         )
+
         self.protocol.save()
         # self.step = Step.objects.create(
         #     name = "Test Step",
@@ -35,17 +49,18 @@ class ProtocolViewTests(TestCase):
 
     def test_create_protocol(self):
 
-        url = reverse("protocol_create", kwargs={'owner_slug': self.organization.slug})
+        url = reverse("protocol_create", kwargs={'owner_slug': self.org.slug})
         self.assertTrue(self.client.login(username='testuser', password='password'))
         
         data = dict(name="Test Protoco 2l", raw="blag nlag")
         response = self.client.post(url, data, follows=True)
         
-    # def test_protocol_detail(self):
-        
+    # def test_protocol_detail(self):        
     #     self.assertTrue(self.client.login(username='testuser', password='password'))
     #     url = self.protocol.get_absolute_url()
     #     response = self.client.get(url)
     #     self.assertContains(response, '<td><a href="/protocols/test-protocol/steps/test-step/">Test Step</a></td>')
         
-        
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
