b8bafcc103acc20e2cbbfb29c082bd2a432eecc9
orenis1 <orenis1@gmail.com>
2013-03-06 01:16:01 -0800

    cleaned up more views

--- bnbapp/bionetbook/compare/urls.py
+++ bnbapp/bionetbook/compare/urls.py
@@ -4,10 +4,10 @@ from compare import views
 
 urlpatterns = patterns("",
     #url(regex=r'^(?P<protocol_slug>[-\w]+).svg$', view=protocol_plot, name='compare_protocol_plot'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.CompareBaseView.as_view(), name='compare_protocols'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareBaseGraphicView.as_view(), name='compare_protocol_graphic'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layers/(?P<layers>[-\w]+)/$', view=views.CompareLayersView.as_view(), name='compare_add_layers'),
-    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layers/(?P<layers>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareLayersGraphicView.as_view(), name='compare_add_layers_graphic'),
+    # url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.CompareBaseView.as_view(), name='compare_protocols'),
+    # url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareBaseGraphicView.as_view(), name='compare_protocol_graphic'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layers/(?P<layers>[-\w]+)/$', view=views.CompareLayersView.as_view(), name='compare_layers'),
+    url(regex=r'^(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/layers/(?P<layers>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareLayersGraphicView.as_view(), name='compare_layers_graphic'),
     url(regex=r'^(?P<protocol_a_slug>[-\w]+)/layers/(?P<layers>[-\w]+)/$', view=views.CompareSingleLayersView.as_view(), name='compare_single_layers'),
     url(regex=r'^(?P<protocol_a_slug>[-\w]+)/layers/(?P<layers>[-\w]+).(?P<format>[-\w]+)$', view=views.CompareSingleLayersGraphicView.as_view(), name='compare_single_layers_graphic')
     # url(regex=r'^(?P<protocol_a_slug>[-\w]+)/base/single/(?P<layers>[-\w]+)/$', view=views.SingleBaseView.as_view(), name='draw_single_base'),
