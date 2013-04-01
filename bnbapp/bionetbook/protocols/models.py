bb6efebe5be1c6a1a4ec1f0797e6120931f18393
Grant Viklund <gviklund@backcode.com>
2013-03-31 17:49:51 -0700

    Adding Models for Workflow management

--- bnbapp/bionetbook/protocols/models.py
+++ bnbapp/bionetbook/protocols/models.py
@@ -928,21 +928,37 @@ class Step(NodeBase):
     #     return 
 
 
+class Workflow(TimeStampedModel):
+    name = models.CharField(_("Name"), max_length=255, unique=True)
+    author = models.ForeignKey(User, blank=True, null=True)
+    owner = models.ForeignKey(Organization)
+    protocols = models.ManyToManyField(Protocol, through='WorkflowProtocol')
+    slug = models.SlugField(_("Slug"), blank=True, null=True, max_length=255)
+    duration = models.IntegerField(_("Duration in seconds"), blank=True, null=True)
+    description = models.TextField(_("Description"), blank=True, null=True)
+    note = models.TextField(_("Notes"), blank=True, null=True)
 
-# class ProtocolIngest(Protocol):
-#     '''Used for loading protocols from JSON formatted text files.'''
 
-#     class Meta:
-#         db_table = 'protocols_protocol'
+class WorkflowProtocol(TimeStampedModel):
+    protocol = models.ForeignKey(Protocol)
+    workflow = models.ForeignKey(Workflow)
+    order = models.IntegerField(_("Order or Protocols"), blank=True, null=True)
 
-#     def read_data(self, *args):
-#         filename = str(args[0])
-#         self.data = open(filename, 'r').read()
-#         if self.data:
-#             return self.data
-#         else:
-#             print 'no data loaded'
+    def __unicode__(self):
+        return self.protocol.name + " - " + self.workflow.name
 
+
+#   Commenting out for now until this is formally ready to be added and all the fields are worked out
+#
+# class Experiment(TimeStampedModel):
+#     experimentist = models.ForeignKey(User, blank=True, null=True)
+#     workflow = models.ForeignKey(Workflow)
+#     owner = models.ForeignKey(Organization)
+#     name = models.CharField(_("Name"), max_length=255, unique=True)
+#     slug = models.SlugField(_("Slug"), blank=True, null=True, max_length=255)
+#     duration = models.IntegerField(_("Duration in seconds"), blank=True, null=True)
+#     description = models.TextField(_("Description"), blank=True, null=True)
+#     experiment_data = JSONField(blank=True, null=True)
 
 
 
