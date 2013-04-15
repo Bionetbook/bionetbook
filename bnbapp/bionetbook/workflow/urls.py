dd553c0ba9f63923af23bdfc1b4c331ee97d4a56
Grant Viklund <gviklund@backcode.com>
2013-04-14 17:39:45 -0700

    Adding Workflow Create

--- bnbapp/bionetbook/workflow/urls.py
+++ bnbapp/bionetbook/workflow/urls.py
@@ -3,8 +3,8 @@ from workflow import views
 
 urlpatterns = patterns("",
     url(regex=r'^(?P<owner_slug>[-\w]+)/$', view=views.WorkflowListView.as_view(), name='workflow_list'),
+    url(regex=r'^(?P<owner_slug>[-\w]+)/add/$', view=views.WorkflowCreateViewBase.as_view(), name='workflow_create'),
     url(regex=r'^(?P<owner_slug>[-\w]+)/(?P<workflow_slug>[-\w]+)/$', view=views.WorkflowDetailView.as_view(), name='workflow_detail'),
-    url(regex=r'^(?P<owner_slug>[-\w]+)/(?P<workflow_slug>[-\w]+)/add/$', view=views.WorkflowCreateViewBase.as_view(), name='workflow_create'),
     url(regex=r'^(?P<owner_slug>[-\w]+)/(?P<workflow_slug>[-\w]+)/edit/$', view=views.WorkflowUpdateView.as_view(), name='workflow_update'),
     url(regex=r'^(?P<owner_slug>[-\w]+)/(?P<workflow_slug>[-\w]+)/delete/$', view=views.WorkflowDeleteView.as_view(), name='workflow_delete'),
 )
