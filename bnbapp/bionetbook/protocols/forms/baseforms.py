00aacfac95a8fc870a65b0383fcb7a798facbb59
orenis1 <orenis1@gmail.com>
2013-04-15 17:00:54 -0700

    added data back to protocol input fields

--- bnbapp/bionetbook/protocols/forms/baseforms.py
+++ bnbapp/bionetbook/protocols/forms/baseforms.py
@@ -6,7 +6,7 @@ class ProtocolForm(forms.ModelForm):
 
     class Meta:
         model = Protocol
-        exclude = ('parent', 'slug', 'duration_in_seconds', 'status','raw', 'data', 'author', 'owner', 'published', 'public')
+        exclude = ('parent', 'slug', 'duration_in_seconds', 'status','raw', 'author', 'owner', 'published', 'public') # 'data',
 
     # def __init__(self, *args, **kwargs):
     #     super(ProtocolForm, self).__init__(*args, **kwargs)
