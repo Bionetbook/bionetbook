86eb03002e9f443ae93629be42ee766863fd02d5
orenis1 <orenis1@gmail.com>
2013-04-16 21:28:28 -0700

    cleaned up link

--- bnbapp/bionetbook/protocols/forms/verbs/call_for_protocol.py
+++ bnbapp/bionetbook/protocols/forms/verbs/call_for_protocol.py
@@ -9,6 +9,7 @@ class CallForProtocolForm(forms.VerbForm):
 
     protocol_type = forms.CharField(required = False, help_text = 'describe the basic chemistry')
     protocol_kit_name = forms.CharField(required = False, help_text = 'kit name')
+    protocol_link = forms.CharField(required=False, help_text='enter the protocol URL, add both / at the beginning and end')
     input_to_track = forms.CharField(help_text = 'reagent, sample, molecule, compounds, strain etc.')
     input_notes = forms.CharField(required = False, help_text = 'concentration, volume, mass etc')
     output_to_track = forms.CharField(help_text = 'reagent, sample, molecule, compounds, strain etc.')
