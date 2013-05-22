567687f954169558d10d2468a80fc6d63d6dd5e7
orenis1 <orenis1@gmail.com>
2013-05-22 11:34:13 -0700

    added json methods to protocols.models

--- bnbapp/bionetbook/protocols/models.py
+++ bnbapp/bionetbook/protocols/models.py
@@ -438,6 +438,45 @@ class Protocol(TimeStampedModel):
     def update_duration(self):
         pass
 
+    def action_children_json(self):
+        out = []
+        for action in self.get_actions:
+            children = self.nodes[action].children
+            if children:
+                if type(children) is list:
+                    temp = []
+                    for child in children:
+                        temp.append(child['objectid'])
+                    out.append({action: temp})
+                if type(children) is not list:
+                    out.append({action: children['objectid']})
+            else: 
+                out.append({action: None})
+        return out
+
+
+    def protocol_tree_json(self):
+        out = []
+        for step in self.get_steps:
+            step_dict={}
+            step_dict[step] = []
+            actions = [r['objectid'] for r in self.nodes[step].children] 
+            for action in actions:
+                action_dict = {}
+                children = self.nodes[action].children
+                if children:
+                    if type(children) is list:
+                        action_dict[action] = [r['objectid'] for r in self.nodes[action].children]
+                    else: 
+                         action_dict[action] = children['objectid']       
+                else: 
+                    action_dict[action] = None
+
+                step_dict[step].append(action_dict)    
+                
+            out.append(step_dict)
+        return out        
+
 
 class NodeBase(dict):
     """Base class for the protocol components"""
