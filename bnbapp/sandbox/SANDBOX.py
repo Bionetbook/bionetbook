e5b8fb7c3f88c992783575654f3220623ae0805c
orenis1 <orenis1@gmail.com>
2013-05-23 09:57:38 -0700

    updated sandbox

--- bnbapp/sandbox/SANDBOX.py
+++ bnbapp/sandbox/SANDBOX.py
@@ -1,53 +1,105 @@
-SANDBOX	
+SANDBOX
+def action_children(self):
+    out = []
+    for action in self.get_actions:
+        children = self.nodes[action].children
+        if children:
+            if type(children) is list:
+                temp = []
+                for child in children:
+                    temp.append(child['objectid'])
+                out.append({action: temp})
+            if type(children) is not list:
+                out.append({action: children['objectid']})
+        else: 
+            out.append({action: None})
+
+out = []
+for step in c.get_steps:
+step_dict = {}
+step_dict[step] = []
+    actions = c.nodes[step].children    
+    for action in actions:
+        children = list(c.nodes[action].children)
+        if children:
+            if type(children) is list:
+                temp = []
+                for child in children:
+                    temp.append(child['objectid'])
+                out.append({action: temp})
+            if type(children) is not list:
+                out.append({action: children['objectid']})
+        else: 
+            out.append({action: None})
+
+make list
+cycle through step ids
+create step dictionary:
+    step_dict = {}
+    stepdict[step] = []
+    for action in step:
+        actiondict = {}
+        actiondict[action] = []
+        for node in action:
+            nodedict = {}
+            nodedict[node] = []
+
+        stepdict[step].append(actiondict) 
 
 
-	c.same_layer_objects = c.get_reagents_by_action()	
-	c.same_layer_objects_lit = c.get_reagents_by_action('literal')	
-	
-	# define the subgraph with a dict that groups objects into a single rank 
-c.rank_objects = {} 
-for i in c.same_layer_objects:
-	c.rank_objects[i] = []
-	c.rank_objects[i].append(c.same_layer_objects[i][0])
-	c.rank_objects[i].append(i)
-	c.agraph.add_edges_from([(i, c.rank_objects[i][0]) for i in c.rank_objects])
+out =[]
+for step in c.get_steps:
+    step_dict={}
+    step_dict[step] = []
+    actions = [r['objectid'] for r in c.nodes[step].children]
+    for action in actions:
+        action_dict = {}
+        action_dict[action] = []
+        children = [r['objectid'] for r in c.nodes[step].children]
+        if children:
+            if type(children) is list:
+                temp = []
+                for child in children:
+                    temp.append(child['objectid'])
+                action_dict[action].append({action: temp})
+            if type(children) is not list:
+                action_dict[action].append({action: children['objectid']})
+        else: 
+            action_dict[action].append({action: None})
 
-	# build all subgraphs:
-names=['a1','a2','a3','a4','a5','a6','a7'] # automate to protName_verb for pairwise comparisson
-nc=0
-for i in c.rank_objects:
-	N = c.agraph.add_subgraph(c.rank_objects[i], name='%s'%(names[nc]), rank = 'same', rankdir='LR')
-	nc+=1
-	# label the nodes in the subgraph of the current layer:
-	for i in c.rank_objects:
-		n = c.agraph.get_node(c.rank_objects[i][0]) # get rank node that links to base node for each rank
-		# e = c.agraph.get_edge(c.rank_objects[i][0], c.rank_objects[i][1]) # fix this
-		v = c.same_layer_objects[i] # get a list of all nodes of this subgraph
-		n.attr['shape'] = 'record'
-		
-		''' assemble the label:
-		remove commas,  - done
-		attach measurement units -not yet
-		add kwargs here
-		'''	
-		label_assembly = []
+    out.append(step_dict)
 
-		for k in range(len(v)): # rename all reagents in an action
-			tmp = c.objectid2name(v[k], reagents=True, units=True)
-			name = tmp['name'].replace(',','')
-			units = tmp['units']
-			label_assembly.append(name + ' ' + units)
 
-		
-		n.attr['label'] = '{' + ' | '.join(label_assembly) +'}'
+def protocol_tree_json(self):
+    out = []
+    for step in self.get_steps:
+        step_dict={}
+        step_dict[step] = []
+        actions = [r['objectid'] for r in self.nodes[step].children] 
+        for action in actions:
+            action_dict = {}
+            children = self.nodes[action].children
+            if children:
+                if type(children) is list:
+                    action_dict[action] = [r['objectid'] for r in self.nodes[action].children]
+                else: 
+                     action_dict[action] = children['objectid']       
+            else: 
+                action_dict[action] = None
 
-for step in c.steps:
-    result[step['objectid']] = step
-    print step['objectid']
-    for action in step['actions']:
-        result[action['objectid']] = action
-        print '-' + action['objectid']	
-        if 'component - list' in action:
-            for component in action['component - list']:
-                result[component['objectid']] = component
-                print '--' + component['objectid']		
+            step_dict[step].append(action_dict)    
+            
+        out.append(step_dict)
+
+
+
+
+
+
+class tree(object):
+
+
+
+
+
+
