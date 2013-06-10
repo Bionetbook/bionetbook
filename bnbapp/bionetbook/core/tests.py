2c472b78d21b021dd67f4cb3fc422b19245d8cf4
Grant Viklund <gviklund@backcode.com>
2013-06-09 23:41:58 -0700

    Trying to move AutoBaseTest over to core lib

--- bnbapp/bionetbook/core/tests.py
+++ bnbapp/bionetbook/core/tests.py
@@ -6,11 +6,28 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
+class AutoBaseTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def createUserInstance(self, **kwargs):
+        '''Method to create a user and register it with the teardown operation'''
+        m = User.objects.create_user( **kwargs )
+        self.teardownList.append(m)
+        return m
+
+    def createModelInstance(self, model, **kwargs):
+        '''Method to create any Model instance and register it with the teardown operation'''
+        m = model( **kwargs )
+        m.save()
+        self.teardownList.append(m)
+        return m
+
+    def setUp(self):
+        self.teardownList = []
+
+    def tearDown(self):
+        self.teardownList.reverse()     # Reverse the list's creation order
+
+        for item in self.teardownList:
+            item.delete()
