7a84ef67c871534014d3f49a897aa05324bd4b89
orenis1 <orenis1@gmail.com>
2013-06-17 16:47:18 -0700

    cleaned up api/urls

--- bnbapp/bionetbook/api/urls.py
+++ bnbapp/bionetbook/api/urls.py
@@ -5,15 +5,14 @@ from api import views
 
 urlpatterns = patterns("",
 	#url(regex=r'^v1/protocol/(?P<protocol_slug>[-\w]+)$', view='api.views.protocol_detail', name='api_detail'),
+	url(regex=r'^json/manual_data/$', view='api.views.json_manual_data', name='api_protocol_test_json'),
+	url(regex=r'^json/classbased/(?P<protocol_slug>[-\w]+)/$', view=views.JsonTestView.as_view(), name='json_template_test_view'),
 	url(regex=r'^jqtest/$', view=views.JQTestView.as_view(), name='api_jqtest'),		# FAILS
-	url(regex=r'^protocoltest/(?P<protocol_a_slug>[-\w]+)/$', view=views.CompareLayersView.as_view(), name='api_protocoltest'),		# FAILS
-	url(regex=r'^test/(?P<protocol_a_slug>[-\w]+)/$', view=views.TestView.as_view(), name='api_test'),				# WORKS
 	url(regex=r'^json/all/$', view='api.views.json_dump_all', name='api_json_dump_all'),
-	url(regex=r'^json/layers/(?P<protocol_slug>[-\w]+)/$', view='api.views.protocol_layers_json', name='api_protocol_layers_json'),
-	url(regex=r'^json/test/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.protocol_diff_json_aligned', name='api_protocol_compare_layers_json'),
 	url(regex=r'^json/layers/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.protocol_diff_json', name='api_protocol_compare_layers_json'),
 	url(regex=r'^json/(?P<protocol_slug>[-\w]+)/$', view='api.views.protocol_json', name='api_protocol_json'),
 	url(regex=r'^json/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.protocol_compare_json', name='api_protocol_compare_json'),
+
     # url(regex=r'^(?P<owner_slug>[-\w]+)/$', view=views.ProtocolListView.as_view(), name='organization_protocol_list'),
 )
 
