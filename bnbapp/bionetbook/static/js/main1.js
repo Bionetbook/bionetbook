51e8c753dc6d7492880253d6d76efe1915cb45d4
orenis1 <orenis1@gmail.com>
2013-06-18 13:48:25 -0700

    moved and sorted out static aliendev files

---
+++ bnbapp/bionetbook/static/js/main1.js
@@ -0,0 +1,15 @@
+function getFlowchartData(successCallback){
+    $.get("/api/json/manual_data/", function(data) { successCallback(data); });
+};
+
+// $(function(){
+//     getFlowchartData(function(data) {
+//         $('.flowchart-container').flowchart( data );
+//     });
+// });
+
+function callback(data) {
+	$('.flowchart-container').flowchart( data );
+}
+
+getFlowchartData( callback );
