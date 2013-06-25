a817cde4b38bbc06ef45842470872f10470b2af5
orenis1 <orenis1@gmail.com>
2013-06-24 23:22:48 -0700

    updated files for js to json connection

--- bnbapp/bionetbook/static/js/main_api.js
+++ bnbapp/bionetbook/static/js/main_api.js
@@ -7,7 +7,7 @@ function getFlowchartData(){
             slugs += arguments[i] + '/';
     }
 
-    $.get("/api/json/manual_data/" + slugs, function(data) { successCallback(data); });
+    $.get("/api/json/aligned/" + slugs, function(data) { successCallback(data); });
 };
 
 function callback(data) {
