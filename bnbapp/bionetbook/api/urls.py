b02ba8048979c8faa39f9e8ebf6b91be89385760
Grant Viklund <gviklund@backcode.com>
2013-06-25 20:45:49 -0700

    Fixed issue with non-loaded Class found in urls.py

--- bnbapp/bionetbook/api/urls.py
+++ bnbapp/bionetbook/api/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns("",
 	url(regex=r'^view/ajax/(?P<protocol_a_slug>[-\w]+)/$', view=views.AjaxView.as_view(), name='AjaxView'),
 	
 # depricated URLS:
-	url(regex=r'^jqtest/$', view=views.JQTestView.as_view(), name='api_jqtest'),		# FAILS
+	# url(regex=r'^jqtest/$', view=views.JQTestView.as_view(), name='api_jqtest'),		# FAILS
 	url(regex=r'^json/all/$', view='api.views.json_dump_all', name='api_json_dump_all'),
 	url(regex=r'^json/layers/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.protocol_diff_json', name='api_protocol_compare_layers_json'),
 	url(regex=r'^json/(?P<protocol_slug>[-\w]+)/$', view='api.views.protocol_json', name='api_protocol_json'),
