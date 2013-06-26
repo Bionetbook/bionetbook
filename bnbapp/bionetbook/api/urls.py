06e7f253c08840d231243d22cedb7cccd7975cfa
orenis1 <orenis1@gmail.com>
2013-06-26 15:47:56 -0500

    cleaned up api URLs

--- bnbapp/bionetbook/api/urls.py
+++ bnbapp/bionetbook/api/urls.py
@@ -4,20 +4,14 @@ from django.conf.urls.defaults import patterns, url, include
 from api import views
 
 urlpatterns = patterns("",
+	# incomplete ajax:
+	url(regex=r'^view/data_static/$', view=views.AjaxStaticView.as_view(), name='ajax_static_view'),
 	
-	url(regex=r'^view/data_static/$', view=views.JsonStaticView.as_view(), name='JsonManulaView'),
-	url(regex=r'^json/data_dynamic/$', view='api.views.json_data_dynamic', name='json_api_static'),
-	url(regex=r'^view/ajax_dynamic/$', view=views.AjaxDynamicView.as_view(), name='AjaxDynamicView'),
+	url(regex=r'^json/data_dynamic/$', view='api.views.json_data_dynamic', name='json_data_dynamic'),
+	url(regex=r'^view/ajax_dynamic/$', view=views.AjaxDynamicView.as_view(), name='ajax_dynamic_view'),
 	
-	# url(regex=r'^json/ajax/(?P<protocol_a_slug>[-\w]+)/$', view='api.views.get_layout_compare_json', name='get_layout_compare_json'),
 	url(regex=r'^json/ajax/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.get_layout_compare_json_v2', name='get_layout_compare_json_v2'),
 	url(regex=r'^view/ajax/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.AjaxView.as_view(), name='AjaxView'),
-	
-# depricated URLS:
-	# url(regex=r'^jqtest/$', view=views.JQTestView.as_view(), name='api_jqtest'),		# FAILS
-	url(regex=r'^json/all/$', view='api.views.json_dump_all', name='api_json_dump_all'),
-	url(regex=r'^json/layers/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.protocol_diff_json', name='api_protocol_compare_layers_json'),
-	# url(regex=r'^json/(?P<protocol_slug>[-\w]+)/$', view='api.views.protocol_json', name='api_protocol_json'),
 	
 )
 
