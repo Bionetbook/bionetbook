a333a43275d432fa22e283e17061fdb9ebc49916
orenis1 <orenis1@gmail.com>
2013-06-29 14:01:00 -0700

    Commented out old view in API

--- bnbapp/bionetbook/api/urls.py
+++ bnbapp/bionetbook/api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns("",
 	url(regex=r'^view/ajax_dynamic/$', view=views.AjaxDynamicView.as_view(), name='ajax_dynamic_view'),
 	
 	url(regex=r'^json/single_ajax/(?P<protocol_a_slug>[-\w]+)/$', view='api.views.get_layout_json', name='get_layout_json'),
-	url(regex=r'^view/single_ajax/(?P<protocol_a_slug>[-\w]+)/$', view=views.AjaxSingleView.as_view(), name='AjaxSinlgeView'),
+	# url(regex=r'^view/single_ajax/(?P<protocol_a_slug>[-\w]+)/$', view=views.AjaxSingleView.as_view(), name='AjaxSinlgeView'),
 
 	url(regex=r'^json/ajax/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view='api.views.get_layout_compare_json', name='get_layout_compare_json'),
 	# url(regex=r'^view/ajax/(?P<protocol_a_slug>[-\w]+)/(?P<protocol_b_slug>[-\w]+)/$', view=views.AjaxView.as_view(), name='ajax_view'),
