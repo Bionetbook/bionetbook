8f23fd119a4773f6ea5ec83f2b96cb85d7114d22
Grant Viklund <gviklund@backcode.com>
2013-07-14 11:27:15 -0700

    Establishing testing plan

--- bnbapp/bionetbook/protocols/tests/test_security.py
+++ bnbapp/bionetbook/protocols/tests/test_security.py
@@ -10,37 +10,116 @@ from organization.models import Organization, Membership
 
 
 class ProtocolSecurityTests(AutoBaseTest):
+    '''
+    FIRST USER -> ORG 1
+    SECOND USER -> ORG 2
+    THIRD USER -> ORG 1
+
+    FIRST USER CAN ACCESS THIER PRIVATE DRAFT PROTOCOL
+
+    FIRST USER CAN ACCESS THIER PRIVATE PUBLISHED PROTOCOL
+
+    FIRST USER CAN ACCESS THIER PUBLIC DRAFT PROTOCOL
+
+    FIRST USER CAN ACCESS THIER PUBLIC PUBLISHED PROTOCOL
+
+
+    FIRST USER CAN NOT ACCESS SECOND USER'S PRIVATE DRAFT PROTOCOL
+
+    FIRST USER CAN NOT ACCESS SECOND USER'S PRIVATE PUBLISHED PROTOCOL
+
+    FIRST USER CAN NOT ACCESS SECOND USER'S PUBLIC DRAFT PROTOCOL
+
+    FIRST USER CAN ACCESS SECOND USER'S PUBLIC PUBLISHED PROTOCOL
+
+
+    FIRST USER CAN NOT ACCESS THIRD USER'S PRIVATE DRAFT PROTOCOL 
+
+    FIRST USER CAN ACCESS THIRD USER'S PRIVATE PUBLISHED PROTOCOL
+
+    FIRST USER CAN NOT ACCESS THIRD USER'S PUBLIC DRAFT PROTOCOL
+
+    FIRST USER CAN ACCESS THIRD USER'S PUBLIC PUBLISHED PROTOCOL
+    '''
 
     def setUp(self):
         super(ProtocolSecurityTests, self).setUp()
 
         # USER SETUP
-        self.user = self.createUserInstance(username="testuser", password="pass", email="test@example.com")
-        self.userTwo = self.createUserInstance(username="usertwo", password="pass", email="usertwo@example.com")
+        self.firstUser = self.createUserInstance(username="firstuser", password="pass", email="firstuser@example.com")
+        self.secondUser = self.createUserInstance(username="seconduser", password="pass", email="seconduser@example.com")
+        self.thirdUser = self.createUserInstance(username="thirduser", password="pass", email="thirduser@example.com")
 
         # USER PROFILE SETUP
-        self.profile = self.createModelInstance(Profile, user=self.user)
-        self.profileTwo = self.createModelInstance(Profile, user=self.userTwo)
+        self.firstProfile = self.createModelInstance(Profile, user=self.firstUser)
+        self.secondProfile = self.createModelInstance(Profile, user=self.secondUser)
+        self.thirdProfile = self.createModelInstance(Profile, user=self.thirdUser)
 
         # CREATE THE ORGANIZATION
-        self.org = self.createModelInstance(Organization, name="TestOrg")
-        self.orgTwo = self.createModelInstance(Organization, name="TestTwoOrg")
+        self.firstOrg = self.createModelInstance(Organization, name="FirstOrg")
+        self.secondOrg = self.createModelInstance(Organization, name="SecondOrg")
 
         # ADD THE MEMBERSHIP
-        self.member = self.createModelInstance(Membership, user=self.user, org=self.org)
-        self.memberTwo = self.createModelInstance(Membership, user=self.userTwo, org=self.orgTwo)
+        self.firstMember = self.createModelInstance(Membership, user=self.firstUser, org=self.firstOrg)
+        self.secondMember = self.createModelInstance(Membership, user=self.secondUser, org=self.secondOrg)
+        self.thirdMember = self.createModelInstance(Membership, user=self.thirdUser, org=self.firstOrg)
 
         # CREATE PROTOCOL
-        self.protocol = self.createModelInstance(Protocol, name="Test Protocol", owner=self.org)
+        self.firstPrivateDraftProtocol = self.createModelInstance(Protocol, name="firstPrivateDraftProtocol", owner=self.firstOrg, published=False, public=False)
+        self.firstPrivatePublishedProtocol = self.createModelInstance(Protocol, name="firstPrivatePublishedProtocol", owner=self.firstOrg, published=True, public=False)
+        self.firstPublicDraftProtocol = self.createModelInstance(Protocol, name="firstPublicDraftProtocol", owner=self.firstOrg, published=False, public=True)
+        self.firstPublicPublishedProtocol = self.createModelInstance(Protocol, name="firstPublicPublishedProtocol", owner=self.firstOrg, published=True, public=True)
+
+        self.secondPrivateDraftProtocol = self.createModelInstance(Protocol, name="secondPrivateDraftProtocol", owner=self.secondOrg, published=False, public=False)
+        self.secondPrivatePublishedProtocol = self.createModelInstance(Protocol, name="secondPrivatePublishedProtocol", owner=self.secondOrg, published=True, public=False)
+        self.secondPublicDraftProtocol = self.createModelInstance(Protocol, name="secondPublicDraftProtocol", owner=self.secondOrg, published=False, public=True)
+        self.secondPublicPublishedProtocol = self.createModelInstance(Protocol, name="secondPublicPublishedProtocol", owner=self.secondOrg, published=True, public=True)
+
+        self.thirdPrivateDraftProtocol = self.createModelInstance(Protocol, name="thirdPrivateDraftProtocol", owner=self.firstOrg, published=False, public=False)
+        self.thirdPrivatePublishedProtocol = self.createModelInstance(Protocol, name="thirdPrivatePublishedProtocol", owner=self.firstOrg, published=True, public=False)
+        self.thirdPublicDraftProtocol = self.createModelInstance(Protocol, name="thirdPublicDraftProtocol", owner=self.firstOrg, published=False, public=True)
+        self.thirdPublicPublishedProtocol = self.createModelInstance(Protocol, name="thirdPublicPublishedProtocol", owner=self.firstOrg, published=True, public=True)
 
     def test_login(self):
-        url = reverse("protocol_create", kwargs={'owner_slug': self.org.slug})
-        self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.user is the first user
+        url = reverse("protocol_create", kwargs={'owner_slug': self.firstOrg.slug})
+        self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.firstUser is the first user
+
+
+    # FIRST USER -> ORG 1
+    # SECOND USER -> ORG 2
+    # THIRD USER -> ORG 1
+
+    # FIRST USER CAN ACCESS THIER PRIVATE DRAFT PROTOCOL
+
+    # FIRST USER CAN ACCESS THIER PRIVATE PUBLISHED PROTOCOL
+
+    # FIRST USER CAN ACCESS THIER PUBLIC DRAFT PROTOCOL
+
+    # FIRST USER CAN ACCESS THIER PUBLIC PUBLISHED PROTOCOL
+
+
+    # FIRST USER CAN NOT ACCESS SECOND USER'S PRIVATE DRAFT PROTOCOL
+
+    # FIRST USER CAN NOT ACCESS SECOND USER'S PRIVATE PUBLISHED PROTOCOL
+
+    # FIRST USER CAN NOT ACCESS SECOND USER'S PUBLIC DRAFT PROTOCOL
+
+    # FIRST USER CAN ACCESS SECOND USER'S PUBLIC PUBLISHED PROTOCOL
+
+
+    # FIRST USER CAN NOT ACCESS THIRD USER'S PRIVATE DRAFT PROTOCOL 
+
+    # FIRST USER CAN ACCESS THIRD USER'S PRIVATE PUBLISHED PROTOCOL
+
+    # FIRST USER CAN NOT ACCESS THIRD USER'S PUBLIC DRAFT PROTOCOL
+
+    # FIRST USER CAN ACCESS THIRD USER'S PUBLIC PUBLISHED PROTOCOL
+
 
     # def test_user_has_access(self):
-    #     url = reverse("protocol_create", kwargs={'owner_slug': self.org.slug})
-    #     self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.user is the first user
+    #     url = reverse("protocol_create", kwargs={'owner_slug': self.firstOrg.slug})
+    #     self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.firstUser is the first user
 
     # def test_user_has_no_access(self):
-    #     url = reverse("protocol_create", kwargs={'owner_slug': self.org.slug})
-    #     self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.user is the first user
+    #     url = reverse("protocol_create", kwargs={'owner_slug': self.firstOrg.slug})
+    #     self.assertTrue(self.client.login(username='testuser', password='pass'))    # Confirm that self.firstUser is the first user
