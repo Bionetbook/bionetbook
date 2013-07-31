c46a943dad3f580593bbc7251d9431dab99b6164
Oren Schaedel <orenis1@gmail.com>
2013-07-31 16:03:05 -0700

    Silenced verbs that are not in use

--- bnbapp/bionetbook/protocols/forms/verbs/__init__.py
+++ bnbapp/bionetbook/protocols/forms/verbs/__init__.py
@@ -1,77 +1,77 @@
-from cook import CookForm
+# from cook import CookForm
 from add import AddForm
 from adjust import AdjustForm
 from agitate import AgitateForm
 from aliquot import AliquotForm
 from apply_ import ApplyForm
-from attach import AttachForm
+# from attach import AttachForm
 from call_for_protocol import CallForProtocolForm
 from centrifuge import CentrifugeForm
 from check import CheckForm
 from chill import ChillForm
-from close import CloseForm
-from collect import CollectForm
+# from close import CloseForm
+# from collect import CollectForm
 from combine import CombineForm
-from connect import ConnectForm
+# from connect import ConnectForm
 # from cool import CoolForm
-from cover import CoverForm
+# from cover import CoverForm
 from cut import CutForm
 from decant import DecantForm
-from digest import DigestForm
+# from digest import DigestForm
 from discard import DiscardForm
-from drain import DrainForm
-from dry import DryForm
+# from drain import DrainForm
+from dry import DryFormB
 from electrophorese import ElectrophoreseForm
 from elute import EluteForm
 # from equilibrate import EquilibrateForm
 from excise import ExciseForm
-from extract import ExtractForm
-from filter import FilterForm
+# from extract import ExtractForm
+# from filter import FilterForm
 from quickfreeze import QuickFreezeForm
 from grind import GrindForm
-from grow import GrowForm
-from harvest import HarvestForm
+# from grow import GrowForm
+# from harvest import HarvestForm
 from heat import HeatForm
 from incubate import IncubateForm
 from insert import InsertForm
-from invert import InvertForm
-from keep import KeepForm
+# from invert import InvertForm
+# from keep import KeepForm
 from let_sit_stand import LetSitStandForm
 from load import LoadForm
 from measure import MeasureForm
-from melt import MeltForm
+# from melt import MeltForm
 # from microcentrifuge import MicrocentrifugeForm
 from mix import MixForm
-from open_ import OpenForm
-from pass_ import PassForm
+# from open_ import OpenForm
+# from pass_ import PassForm
 # from pcr import PcrForm
-from photograph import PhotographForm
+# from photograph import PhotographForm
 from pipette_out import PipetteOutForm
 from place import PlaceForm
-from pour import PourForm
-from pour_off import PourOffForm
+# from pour import PourForm
+# from pour_off import PourOffForm
 from precipitate import PrecipitateForm
 from prepare import PrepareForm
-from purify import PurifyForm
-from recover import RecoverForm
+# from purify import PurifyForm
+# from recover import RecoverForm
 from remove import RemoveForm
 from repeat import RepeatForm
 from resuspend import ResuspendForm
-from rinse import RinseForm
-from seal import SealForm
-from shake import ShakeForm
+# from rinse import RinseForm
+# from seal import SealForm
+# from shake import ShakeForm
 from spin import SpinForm
-from split import SplitForm
-from spool import SpoolForm
-from stop import StopForm
+# from split import SplitForm
+# from spool import SpoolForm
+# from stop import StopForm
 from store import StoreForm
-from swirl import SwirlForm
-from switch_off import SwitchOffForm
+# from swirl import SwirlForm
+# from switch_off import SwitchOffForm
 from thaw import ThawForm
 from thermocycle import ThermocycleForm
 from transfer import TransferForm
-from visualize import VisualizeForm
+# from visualize import VisualizeForm
 from vortex import VortexForm
 from wash import WashForm
-from weigh import WeighForm
-from withdraw import WithdrawForm
+# from weigh import WeighForm
+# from withdraw import WithdrawForm
