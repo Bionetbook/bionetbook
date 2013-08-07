7298fefef05c26e60ad0bc1809506db38d579c3b
AndrewMori <andrewmori@backcode.com>
2013-08-07 16:29:01 -0700

    Added Schedule tab

--- bnbapp/bionetbook/schedule/views.py
+++ bnbapp/bionetbook/schedule/views.py
@@ -1,1 +1,38 @@
 # Create your views here.
+from django import forms, http
+from django.http import Http404
+from django.views.generic import ListView
+from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+from braces.views import LoginRequiredMixin
+from core.views import AuthorizedOrganizationMixin, AuthorizedOrganizationEditMixin, ConfirmationObjectView
+
+from protocols.models import Protocol, Step, Action, Thermocycle, Machine, Component
+from organization.models import Organization
+from schedule.models import Schedule, Event
+
+from protocols.utils import VERB_CHOICES, VERB_FORM_DICT
+
+class ScheduleListView(LoginRequiredMixin, ListView):
+	model = Organization
+	template_name = "schedule/schedule_list.html"
+	slug_url_kwarg = "owner_slug"
+
+	def get_queryset(self):
+		slug = self.kwargs.get(self.slug_url_kwarg, None)
+
+		if slug:
+			self.object = Organization.objects.get(slug=slug)
+			return Organization.objects.filter(slug=slug)
+		else:
+			if self.request.user.is_superuser or self.request.user.is_staff:
+				return Organization.objects.all()
+			if self.request.user.is_authenticated():
+				return self.request.user.organization_set.all()
+			return []
+
+
+	def get_context_data(self, **kwargs):
+		context = super(ScheduleListView, self).get_context_data(**kwargs)
+		context['organization'] = self.object
+		return context
