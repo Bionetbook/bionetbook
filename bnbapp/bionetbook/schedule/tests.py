e59cb5fa7f167bfa9ce54867f52ba07614f2ef38
AndrewMori <andrewmori@backcode.com>
2013-08-08 18:00:39 -0700

    dataToCalendar method

--- bnbapp/bionetbook/schedule/tests.py
+++ bnbapp/bionetbook/schedule/tests.py
@@ -10,8 +10,12 @@ from django.test import TestCase
 from profiles.models import Profile
 from protocols.models import Protocol, Step, Action, Component
 from organization.models import Organization, Membership
+from schedule.models import Calendar
 
 
 class ScheduleViewTests(TestCase):
-	def test_default(self):
-		self.assertEqual(1,1)
+	def CalendarModelTest(self):
+		cal = Calendar()
+		cal.data = {'steps':[{'actions':[{'name':'mix','verb':'mix'}]},{'actions':[{'name':'add','verb':'add'}]}]}
+		output = cal.dataToCalendar()
+		print output
