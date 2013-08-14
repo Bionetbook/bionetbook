6e02ed313ef541789b2687e9bbc5cfa649a38abc
Oren Schaedel <orenis1@gmail.com>
2013-08-14 12:36:20 -0700

    updated apply_ verb

--- bnbapp/bionetbook/protocols/forms/verbs/apply_.py
+++ bnbapp/bionetbook/protocols/forms/verbs/apply_.py
@@ -6,10 +6,10 @@ class ApplyForm(forms.VerbForm):
     name = "Apply"
     slug = "apply"
 
-    duration = forms.IntegerField(help_text='this is the minimal time this should take')
-    edit_to_what = forms.CharField(required = False, help_text = 'sample, mastermix, tube, etc')
+    # duration = forms.IntegerField(help_text='this is the minimal time this should take')
+    # edit_to_what = forms.CharField(required = False, help_text = 'sample, mastermix, tube, etc')
+    item_to_act = forms.CharField(required=False, help_text='what are you applying', label='item to apply')
+    target = forms.CharField(required=False, help_text='where are you placing it')
     describe_where = forms.CharField(required = False, help_text = 'bench, desktop, rotator, etc')
-    remarks = forms.CharField(required = False)
     specify_tool = forms.CharField(required = False, help_text = 'not machine, scissors, pippete, blade etc')
-    comment_why = forms.CharField(required = False)
     number_of_times = forms.IntegerField(required = False)
