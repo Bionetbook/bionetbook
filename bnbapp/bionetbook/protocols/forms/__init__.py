8fcac687ee0bcf52d104db1f70816c8fb4f41acb
Grant Viklund <gviklund@backcode.com>
2013-08-21 10:37:18 -0700

    Added validator for time validation in forms

--- bnbapp/bionetbook/protocols/forms/__init__.py
+++ bnbapp/bionetbook/protocols/forms/__init__.py
@@ -1,7 +1,36 @@
 import floppyforms as forms
 from organization.models import Organization
 from core.utils import CONCENTRATION_UNITS, VOLUME_UNITS, TIME_UNITS, SPEED_UNITS, TEMPERATURE_UNITS, MASS_UNITS, AMMOUNT_UNITS
+from django.core.exceptions import ValidationError
 
+###########
+# REGEX PATTERNS
+###########
+
+validTimeFormat = re.compile('(\d+\:\d+\:\d+)|(\d+\:\d+)|(\d+)')
+
+###########
+# VALIDATORS
+###########
+
+def validateTimeFormat(value):
+    error = False
+    times = value.split("-")
+
+    if len(times) > 2:
+        error = True
+
+    for time in times:
+        if not validTimeFormat.match(time):
+            error = True
+
+    if error:
+        raise ValidationError(u"'%s' has to be in a common time format such as 'Seconds', 'MM:SS', 'HH:MM:SS' or range such as 'maxtime-mintime'" % value)
+
+
+###########
+# FROMS
+###########
 
 class NodeForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
@@ -25,12 +54,6 @@ class StepForm(NodeForm):
 class ActionForm(NodeForm):
     name = forms.CharField(max_length=100, required=False, help_text = 'This will autofill')
     tracking_object = forms.CharField(required=False, help_text='Sample mix | Buffer | Other mix')
-    # tube_label = forms.CharField(help_text ='copy label of tube', required=False)
-    # describe_where = forms.CharField(required = False, help_text = 'bench, desktop, rotator, etc')
-    # min_time = forms.FloatField(required=False, help_text='this is the minimal time this should take', widget=forms.NumberInput(attrs={'step':'any'}))
-    # max_time = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
-    # time_units = forms.ChoiceField(required=False, choices=TIME_UNITS, help_text='in seconds' )
-    # time_comment = forms.CharField(required=False)
     min_temp = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))#, initial = 22.0)
     max_temp = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))#, initial = 22.0)
     temp_units = forms.ChoiceField(required=False, choices=TEMPERATURE_UNITS, help_text='in celcius')
@@ -50,8 +73,6 @@ forms.VerbForm = VerbForm
 
 
 class ComponentForm(NodeForm):
-
-    #reagent_name = forms.CharField(max_length=100, required=False)                 # THIS IS COVERED WITH THE name FIELD
     min_conc = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
     max_conc = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
     conc_units = forms.ChoiceField(required=False, choices=CONCENTRATION_UNITS )
@@ -69,14 +90,9 @@ class ComponentForm(NodeForm):
     source = forms.CharField(required=False, help_text='example: Invitrogen | protocol: RNA purification')
     ph = forms.FloatField(required=False, help_text='only if specified', widget=forms.NumberInput(attrs={'step':'any'}))
     tracking_object = forms.CharField(required=False, help_text='Name for tube: Sample | Mix | Buffer')
-    # tube_label = forms.CharField(help_text ='label of component, not sample', required=False)  
-    # technique_comment = forms.CharField(required=False, help_text='describe technique tips, help, comments, dont describe chemistry or biology')
-    # why = forms.CharField(required=False, help_text='scientific reason you\'re doing this?')
-    # ph = forms.FloatField()
 
 
 class MachineForm(NodeForm):
-
     name = forms.CharField(required=False)
     model = forms.CharField(required=False, help_text='only if it really matters')
     min_time = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
@@ -91,12 +107,9 @@ class MachineForm(NodeForm):
     max_speed = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
     speed_units = forms.ChoiceField(required=False, choices=SPEED_UNITS )
     speed_comment = forms.CharField(required=False)
-    # technique_comment = forms.CharField(required=False, help_text='describe technique tips, help, comments, dont describe chemistry or biology')
-    # why = forms.CharField(required=False, help_text='scientific reason you\'re doing this?')
+
 
 class ThermocyclerForm(NodeForm):
-
-    # phase_name = forms.CharField(required=False, help_text='example: Initiaion denaturation')
     min_temp = forms.FloatField(widget=forms.NumberInput(attrs={'step':'any'}))
     max_temp = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
     temp_units = forms.ChoiceField(required=False, choices=TEMPERATURE_UNITS, help_text='in celcius')
@@ -110,30 +123,3 @@ class ThermocyclerForm(NodeForm):
     cycle_back_to = forms.ChoiceField(required=False, choices=phase_list, initial='')
 
 
-
-'''
-    aliquot_volume = forms.IntegerField()
-    aliquot_concentration = forms.IntegerField()
-    aliquote_what = forms.CharField(help_text = 'name of reagent or mix')
-    apply_action_to = forms.CharField(help_text = 'what are you doing the action on?')    
-    concentration_units = forms.ChoiceField(choices = CONCENTRATION_UNITS)
-    conditional_statement = forms.CharField(required = False, help_text ='if X happens, do Y')
-    number_of_times = forms.IntegerField(required = False)
-    number_of_aliquots = forms.IntegerField(help_text = 'number of tubes you are aliquoting into')
-    min_time = forms.IntegerField()
-    max_time = forms.IntegerField()
-    time_units = forms.ChoiceField(choices = TIME_UNITS, intial = 's')
-    remarks = forms.CharField(required = False)
-    specify_tool = forms.CharField(required = False, help_text = 'not machine, scissors, pippete, blade etc')
-    vessel_type = forms.ChoiceField(required = False, choices = VESSELS)
-    volume_units = forms.ChoiceField(choices = VOLUME_UNITS)
-    using_what = forms.CharField(required = False, help_text = 'rotator, shaker, manual etc')
-
-    to delete:
-    comment_why = forms.CharField(required = False)
-    describe_where = forms.CharField(required = False, help_text = 'bench, desktop, rotator, etc')
-    duration = forms.IntegerField(help_text='this is the minimal time this should take', initial = 'sec')
-    edit_to_what = forms.CharField(required = False, help_text = 'sample, mastermix, tube, etc')
-
-
-'''
