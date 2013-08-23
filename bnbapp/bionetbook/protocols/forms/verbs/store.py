516d0c812e2905a71528275e12b5a7fc88ebed36
Oren Schaedel <orenis1@gmail.com>
2013-08-22 19:42:30 -0700

    minor fixes to verbs

--- bnbapp/bionetbook/protocols/forms/verbs/store.py
+++ bnbapp/bionetbook/protocols/forms/verbs/store.py
@@ -8,7 +8,7 @@ class StoreForm(forms.VerbForm):
     has_manual = True
     layers = ["item_to_act", 'settify']
     
-    item_to_act = forms.CharField(required=False, help_text='what are you removing', label='item to remove')
+    item_to_act = forms.CharField(required=False, label='what are you storing')
     min_time = forms.FloatField(required=False, help_text='this is the minimal time this should take', widget=forms.NumberInput(attrs={'step':'any'}))
     max_time = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
     time_units = forms.ChoiceField(required=False, choices=TIME_UNITS, help_text='in seconds' )
