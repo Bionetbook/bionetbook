1370b2c2997f93bac5e0867d3d01b9b716cc404e
Grant Viklund <gviklund@backcode.com>
2013-08-26 17:54:24 -0700

    Added Reference Table for extra ID tags on Protocols

--- bnbapp/bionetbook/protocols/admin.py
+++ bnbapp/bionetbook/protocols/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from protocols.models import Protocol
+from protocols.models import Protocol, Reference
 
 class ProtocolAdmin(admin.ModelAdmin):
 	list_display = ('slug','public','published','author','owner')
 
+
 admin.site.register(Protocol, ProtocolAdmin)
+admin.site.register(Reference)
