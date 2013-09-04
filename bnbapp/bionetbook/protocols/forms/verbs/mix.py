23a95da5c22256093aa0728a59683b566a71dfa0
Oren Schaedel <orenis1@gmail.com>
2013-09-04 11:38:45 -0700

    fixed duration calculation and initial for seconds in manual verbs

--- bnbapp/bionetbook/protocols/forms/verbs/mix.py
+++ bnbapp/bionetbook/protocols/forms/verbs/mix.py
@@ -10,7 +10,7 @@ class MixForm(forms.VerbForm):
 
     min_time = forms.FloatField(required=False, help_text='this is the minimal time this should take', widget=forms.NumberInput(attrs={'step':'any'}))
     max_time = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
-    time_units = forms.ChoiceField(required=False, choices=TIME_UNITS, help_text='in seconds' )
+    time_units = forms.ChoiceField(required=False, choices=TIME_UNITS, help_text='in seconds', initial = 'sec' )
     time_comment = forms.CharField(required=False)
     # duration_min_time = forms.IntegerField()
     # comment_why = forms.CharField()
