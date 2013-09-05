a63ab2843af53252f15a52343c0e9fc89c59d365
Grant Viklund <gviklund@backcode.com>
2013-09-05 16:34:00 -0700

    Added manual form for Workflow

--- bnbapp/bionetbook/workflow/forms.py
+++ bnbapp/bionetbook/workflow/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from workflow.models import Workflow
-
+from django.db.models.query import EmptyQuerySet
+from django.utils.translation import ugettext_lazy as _
 
 class WorkflowForm(forms.ModelForm):
 
@@ -8,3 +9,12 @@ class WorkflowForm(forms.ModelForm):
         model = Workflow
         #exclude = ('parent', 'slug', 'duration_in_seconds', 'status','raw')
         exclude = ('slug', 'duration', 'author', 'owner', 'protocols')
+
+
+class WorkflowManualForm(forms.Form):
+    # name = forms.CharField( widget=forms.TextInput(attrs={'class':'special'}) )
+    name = forms.CharField( widget=forms.TextInput() )
+    protocols = forms.ModelChoiceField(required=False, queryset=EmptyQuerySet(), label=_("Protocol"), widget=forms.CheckboxSelectMultiple())
+
+    # UPDATE IT IN THE VIEW
+    # form.fields['protocols'] = forms.ModelChoiceField( queryset=self.request.user.profile.get_published_protocols_qs(), label=_("Protocol"), widget=forms.CheckboxSelectMultiple() )
