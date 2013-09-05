9b09a38024ab55aa76a516238e14684d29d55f2b
Grant Viklund <gviklund@backcode.com>
2013-09-05 16:52:15 -0700

    Moved workflow URLs to the right place

--- bnbapp/bionetbook/workflow/urls.py
+++ bnbapp/bionetbook/workflow/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import patterns, url, include
 from workflow import views
 
 urlpatterns = patterns("",
+    url(regex=r'^(?P<owner_slug>[-\w]+)/workflows/create/$', view=views.WorkflowCreateView.as_view(), name='workflow_create'),
+    url(regex=r'^(?P<owner_slug>[-\w]+)/workflows/(?P<workflow_slug>[-\w]+)/$', view=views.WorkflowDetailView.as_view(), name='workflow_detail'),
+
     #url(regex=r'^(?P<owner_slug>[-\w]+)/$', view=views.WorkflowListView.as_view(), name='workflow_list'),
     #url(regex=r'^(?P<owner_slug>[-\w]+)/add/$', view=views.WorkflowCreateView.as_view(), name='workflow_create'),
     #url(regex=r'^(?P<owner_slug>[-\w]+)/workflows/(?P<pk>[-\w]+)/$', view=views.WorkflowDetailView.as_view(), name='workflow_detail'),
