ad2446f70d2fdc0f54a754964382483676d981ae
AndrewMori <andrewmori@backcode.com>
2013-09-09 13:24:15 -0700

    finished experiment detail view and creation

--- bnbapp/bionetbook/bionetbook/urls.py
+++ bnbapp/bionetbook/bionetbook/urls.py
@@ -20,6 +20,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}, name="login"),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}, 'logout',),
     url(r'^', include('workflow.urls')),
+    url(r'^', include('experiment.urls')),
     url(r'^', include('protocols.urls')),
     url(r'^', include('organization.urls')),
 )
--- bnbapp/bionetbook/experiment/views.py
+++ bnbapp/bionetbook/experiment/views.py
@@ -11,7 +11,7 @@ from core.views import PathMixin
 from django.core.urlresolvers import reverse
 from braces.views import LoginRequiredMixin
 from django.template.defaultfilters import slugify
-from workflow.forms import WorkflowForm, WorkflowManualForm
+from experiment.forms import ExperimentManualForm
 from protocols.models import Protocol, Step, Action, Thermocycle, Machine, Component
 from organization.models import Organization
 from schedule.models import Calendar
@@ -31,12 +31,51 @@ class ExperimentDetailView(LoginRequiredMixin, TemplateView):
 		slug = self.kwargs.get(self.slug_url_kwarg, None)
 		experiment = self.request.user.experiment_set.get(slug=slug)
 		org = self.request.user.organization_set.get(slug=self.kwargs['owner_slug'])
+		workflow = experiment.workflow
 		if experiment:
 			context['experiment'] = experiment
 		else:
 			context['experiment'] = None
+		if workflow:
+			context['workflow'] = workflow
+		else:
+			context['workflow'] = None
 		if org:
 			context['organization'] = org
 		else:
 			context['organization'] = None
 		return context
+
+
+class ExperimentCreateView(PathMixin, LoginRequiredMixin, FormView):
+
+	model = Experiment
+	form_class = ExperimentManualForm
+	slug_url_kwarg = "owner_slug"
+	template_name = "experiment/experiment_form.html"
+
+	def get_success_url(self):
+		return self.get_absolute_url()
+
+	def form_valid(self, form):
+		slug = self.kwargs.get(self.slug_url_kwarg, None)
+		org = Organization.objects.get(slug=slug)
+
+		e = Experiment()
+		e.user = self.request.user
+		e.workflow = Workflow.objects.get(pk=form.cleaned_data['workflows'][0])
+		e.data = {'meta':{}}
+		e.name = form.cleaned_data['name']
+		e.slug = slugify(form.cleaned_data['name'])
+		e.owner = org
+		e.save()
+		return HttpResponseRedirect(e.get_absolute_url())
+
+	def get_form(self, form_class):
+		form = form_class(**self.get_form_kwargs())
+		workflows = Organization.objects.get(slug=self.kwargs['owner_slug']).workflow_set.all()
+		form.fields['workflows'] = forms.ChoiceField(
+			label="Workflows",
+			choices=((x.pk,x) for x in workflows))
+		return form
+
