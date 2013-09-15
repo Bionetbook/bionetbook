23d72ecd20d359503cfbdc81ef1b08ed62185bf7
Grant Viklund <gviklund@backcode.com>
2013-09-15 10:54:46 -0700

    Updates model_save to rebuild the step nodes before saving.

--- bnbapp/bionetbook/protocols/admin.py
+++ bnbapp/bionetbook/protocols/admin.py
@@ -5,6 +5,11 @@ from protocols.models import Protocol, Reference
 class ProtocolAdmin(admin.ModelAdmin):
 	list_display = ('slug','public','published','author','owner')
 
+    def save_model(self, request, obj, form, change):
+        # obj.user = request.user
+        obj.rebuild_steps()		# THIS GETS TWEAKED BY THE ADMIN FORM
+        obj.save()
+
 
 admin.site.register(Protocol, ProtocolAdmin)
 admin.site.register(Reference)
