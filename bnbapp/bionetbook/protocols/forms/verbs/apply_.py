5f573f49912a8ef602acc14ed6080739e6c340c6
Oren Schaedel <orenis1@gmail.com>
2013-09-24 19:11:26 -0700

    updated verb apply and org template for release

--- bnbapp/bionetbook/protocols/forms/verbs/apply_.py
+++ bnbapp/bionetbook/protocols/forms/verbs/apply_.py
@@ -5,6 +5,8 @@ class ApplyForm(forms.VerbForm):
 
     name = "Apply"
     slug = "apply"
+    has_manual = True
+    layers = ['item_to_act', 'target', 'number_of_times','settify']
 
     # duration = forms.IntegerField(help_text='this is the minimal time this should take')
     # edit_to_what = forms.CharField(required = False, help_text = 'sample, mastermix, tube, etc')
@@ -13,3 +15,7 @@ class ApplyForm(forms.VerbForm):
     describe_where = forms.CharField(required = False, help_text = 'bench, desktop, rotator, etc')
     specify_tool = forms.CharField(required = False, help_text = 'not machine, scissors, pippete, blade etc')
     number_of_times = forms.IntegerField(required = False)
+    min_time = forms.FloatField(required=False, help_text='this is the minimal time this should take', widget=forms.NumberInput(attrs={'step':'any'}))
+    max_time = forms.FloatField(required=False, widget=forms.NumberInput(attrs={'step':'any'}))
+    time_units = forms.ChoiceField(required=False, choices=TIME_UNITS, help_text='in seconds', initial = 'sec' )
+    time_comment = forms.CharField(required=False)
